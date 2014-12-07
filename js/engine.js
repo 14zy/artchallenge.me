6da5cb8e7008b9281b0e831b4ddc85898f64b6d7
14zy <just14zy@gmail.com>
2014-12-07 23:13:42 +0300

    mobile css 2

--- js/engine.js
+++ js/engine.js
@@ -689,7 +689,7 @@ function refresh(sign){
   document.getElementById("btn4").style.background = "";
   document.getElementById("btn4").style.borderColor = "";
 
-  document.getElementById("art").src = "loading1.gif";
+  document.getElementById("art").src = "pics/loading.gif";
   getart();
 }
 
